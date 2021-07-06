4f3a8e4b625d105dad0c74da8e7fa2dd8bfe719a
oviedo22 <franco_oviedo_12@hotmail.com>
2021-07-05 22:02:53 -0300

    modelo inicial del proyecto

---
+++ Biblioseca.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioseca.Model;
+
+namespace Biblioseca.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Author author = new Author();
+        
+        }
+    }
+}
